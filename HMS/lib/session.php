b695990b920262f589cc73a3f63737432f57c086
vikumDheemantha <vikum.dheemantha@gmail.com>
2017-04-28 10:50:54 +0530

    fix database connection
    take all database connecion files in to one place.

--- HMS/lib/session.php
+++ HMS/lib/session.php
@@ -1,20 +1,11 @@
 <?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$DB_Name = "hostelms";
-	// Create connection
-	$ses_conn = new mysqli($servername, $username, $password, $DB_Name);
-	// Check connection
-	if ($ses_conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
-	}
+	include 'STD_Con.php';
 	//sesson start
 	session_start();
 	$logedUser = $_SESSION['Loged_User'];
 	$ses_sql = "SELECT * FROM user_Details WHERE UserName = '$logedUser'";
 
-	$ses_result = $ses_conn->query($ses_sql);
+	$ses_result = $conn->query($ses_sql);
 
 
 
@@ -30,15 +21,15 @@
 	if($ses_type!='admin')
 	{
 		$sql_hos="SELECT * FROM hostel_staff WHERE warden_id='$ses_id' OR subwarden_id='$ses_id';"; 
-		$ses_result_hos = $ses_conn->query($sql_hos);
+		$ses_result_hos = $conn->query($sql_hos);
 		$hos_row=$ses_result_hos->fetch_assoc();
 		$hos=$hos_row['hostel_id'];
 		$_SESSION['hostel_id']=$hos;
 	}
-	$ses_conn->close();
+	$conn->close();
 	// if(!isset($ses_conform))
 	// {
-	// 	$ses_conn->close();
+	// 	$conn->close();
 	// 	header('Location: index.php');
 	// }
 ?>
