2ee3336847e73ca23071e68825f637c181a48fb0
vikumDheemantha <vikum.dheemantha@gmail.com>
2017-08-01 01:15:19 +0530

    debug swap function

--- HMS/lib/Hos_swap.php
+++ HMS/lib/Hos_swap.php
@@ -1,4 +1,5 @@
 <?php
+include('STD_Con.php');
 $reg1 = $_POST['regNo'];
 $reg2 = $_POST['reg2'];
 $room1 = $_POST['prev_Room'];
@@ -7,6 +8,11 @@ $room2 = $_POST['new_room_no'];
 if(isset($reg1,$reg2,$room1,$room2))
 {
     $sql1 = "UPDATE student_hostel SET room_no = '$room2' WHERE reg_no = '$reg1'";
-    $sql1 = "UPDATE student_hostel SET room_no = '$room1' WHERE reg_no = '$reg2'";
+    $sql2 = "UPDATE student_hostel SET room_no = '$room1' WHERE reg_no = '$reg2'";
+
+    if($conn->query($sql1)=== true && $conn->query($sql2)=== true)
+    {
+        header("Location: ..\Pages\main_local\roomView.php");
+    }
 }
 ?>
